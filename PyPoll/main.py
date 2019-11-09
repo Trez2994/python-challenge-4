7e8eabbc3b4e358ae2136b81b8b9007fbdae5f93
Troy Fernandez <Troy.Fernandez@gmail.com>
2019-11-09 14:00:28 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,6 +18,10 @@ with open(csvpath, newline='') as csvfile:
     # Read each row of data after the header
     total_votes = 0
     candidates_list = []
+    votes_khan = 0
+    votes_correy = 0
+    votes_li = 0
+    votes_otooley = 0
 
     for row in csvreader:
         #print(row)
@@ -25,7 +29,28 @@ with open(csvpath, newline='') as csvfile:
         candidates_list.append(row[2])
     
     for i in range(len(candidates_list)): 
-        print(candidates_list[i])
+        #print(candidates_list[i])
+        if candidates_list[i] == "Khan":
+            votes_khan = votes_khan + 1
+        if candidates_list[i] == "Correy":
+            votes_correy = votes_correy + 1
+        if candidates_list[i] == "Li":
+            votes_li = votes_li + 1
+        if candidates_list[i] == "O'Tooley":
+            votes_otooley = votes_otooley + 1
+
+    percent_khan = round((votes_khan / total_votes) * 100)
+    percent_correy = round((votes_correy / total_votes) * 100)
+    percent_li = round((votes_li / total_votes) * 100)
+    percent_otooley = round((votes_otooley / total_votes) * 100)
+
+        
     
-    print(total_votes)
+    
+    print("The total number of votes is",total_votes)
+    print("The total number of votes for Khan is",votes_khan,"which is",percent_khan,"% of votes")
+    print("The total number of votes for Correy is",votes_correy,"which is",percent_correy,"% of votes")
+    print("The total number of votes for Li is",votes_li,"which is",percent_li,"% of votes")
+    print("The total number of votes for O'Tooley is",votes_otooley,"which is",percent_otooley,"% of votes")
+
     #print(candiates_list)
