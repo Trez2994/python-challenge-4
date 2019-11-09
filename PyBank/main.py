bd8e1e2c695610164ef26b35e050269b6fff0f28
Troy Fernandez <Troy.Fernandez@gmail.com>
2019-11-09 09:56:08 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,7 @@ NumberMonths = 0
 ProfitLoss = []
 maxprofit = []
 minprofit = []
-
+month = []
 
 csvpath = os.path.join(os.getcwd()+'\\PyBank', 'Resources', 'budget_data.csv')
 
@@ -22,28 +22,38 @@ with open(csvpath, newline='') as csvfile:
 
     # Read each row of data after the header
     sum = 0
+
     for row in csvreader:
         print(row)
         NumberMonths = NumberMonths + 1
         ProfitLoss.append(int(row[1]))
         sum = sum + int(row[1])
-     
+        month.append(row[0])
 
+    month_over_month = []
+    for i in range(1,len(ProfitLoss)):
+        monthly_change = ProfitLoss[i] - ProfitLoss[i-1]
+        month_over_month.append([month[i], monthly_change])
 
+    largest_increase = month_over_month[0][1]
+    largest_increase_month = month_over_month[0][0]
+    largest_decrease = month_over_month[0][1]
+    largest_decrease_month = month_over_month[0][0]
 
-    avg = sum / len(ProfitLoss)
-    min = min(ProfitLoss)
-    max = max(ProfitLoss)
+    for j in range(len(month_over_month)):
+        if month_over_month[j][1] > largest_increase:
+            largest_increase = month_over_month[j][1]
+            largest_increase_month = month_over_month[j][0]
 
-    
+        if month_over_month[j][1] < largest_decrease:
+            largest_decrease = month_over_month[j][1]
+            largest_decrease_month = month_over_month[j][0]
 
+    print(largest_increase, largest_increase_month)
+    print(largest_decrease, largest_decrease_month)
 
-
-
-
-
-print(NumberMonths)
-print("The total profit is :", sum)
-print("The average profit is :", avg)
-print("The lowest profit is :", min)
-print("The greatest profit is :", max)
+# print("The number of months is :", NumberMonths)
+# print("The total profit is :", sum)
+# print("The average profit is :", avg)
+# print("The lowest profit is :", min)
+# print("The greatest profit is :", max)
